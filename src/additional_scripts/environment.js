39444b9807612698d1052a020801888e18d32759
Roland Regacho <regacho80@gmail.com>
2024-04-03 08:30:39 +0800

    test change

--- src/additional_scripts/environment.js
+++ src/additional_scripts/environment.js
@@ -1,12 +1,10 @@
 var ENVIRONMENT = false
 var URL = ''
-var test = ''
 
 if (ENVIRONMENT) {
   URL = 'http://192.168.4.200:81/social_media_api'
 } else {
   URL = 'http://192.168.1.3/social_media_api'
 }
-console.log(test)
 
 export default URL
